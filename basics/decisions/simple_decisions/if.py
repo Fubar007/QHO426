f9746edb478a5554a7f697c7e12594ceb6996119
adrianstefanman <>
2022-03-03 17:50:10 +0000

    else,if,elif

---
+++ basics/decisions/simple_decisions/if.py
@@ -0,0 +1,6 @@
+print("What type of book is this?")
+book = input()
+if book == "adventure":
+  print("\nI like adventure books!")
+
+print("\nFinished reading book")
---
+++ basics/decisions/simple_decisions/if_elif_else.py
@@ -0,0 +1,11 @@
+print("Towards what direction should I paint (up, down, left or right)?")
+direction = input()
+
+if direction == "up":
+  print("I am painting in upward direction")
+elif direction == "down":
+  print("I am paiting in downard direction")
+elif direction == "left":
+  print("I am paiting to the leftward direction")
+else:
+  print("I am paiting to the rightward direction")
