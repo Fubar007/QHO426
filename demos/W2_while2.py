c27fc3c9b0f8f6efa453c68bb2fbc81f82b2b6a7
adrianstefanman <>
2022-03-03 12:08:44 +0000

    while loop True

--- demos/W2_while2.py
+++ demos/W2_while2.py
@@ -1,6 +1,16 @@
-answer = ""
-#=1 doesn't equal
-while answer.lower() != "stop":
-  print("Dormamu, I came to bargain!")
-  answer = input()
-print("You won, dr Strange!")
+# answer = ""
+# =1 doesn't equal
+# while answer.lower() != "stop":
+ # print("Dormamu, I came to bargain!")
+ #  answer = input()
+# print("You won, dr Strange!")
+
+#while True
+
+while True:
+  print("hahaha")
+  a = input()
+  if a == "ratatata":
+    break
+
+#while #break
