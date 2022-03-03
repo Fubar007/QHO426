ef8cfe09ab25795c2294ff0db5e628d72ab3784c
adrianstefanman <>
2022-03-03 12:21:31 +0000

    Range and for function

---
+++ demos/W2_for.py
@@ -0,0 +1,14 @@
+#Range function range(Start=0, End, Step=1)
+#range(6) = 0, 1, 2, 3, 4, 5 aici step e 1 ca nu e indicat
+#range(3, 7) = 3, 4, 5, 6
+#range(2, 12 ,2)= 2, 4, 6, 8, 19 aici step= 2 
+
+for i in range(1, 11):
+  print (f"5x{i} = {5*i}")
+
+
+for i in range(10):
+  print("alloha!")
+
+for i in range(100, 200, 6):
+  print(f"The number is {i}")
