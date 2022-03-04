97b9b9237b3253f84fbd3ac56029fa88d7ae186c
adrianstefanman <>
2022-03-04 17:32:07 +0000

    simple while loop

---
+++ basics/repetitions/while_loop/simple_loop.py
@@ -0,0 +1,6 @@
+print("How many cables should I remove?")
+cables = int(input())
+i=0
+while i < cables:
+  print("Removed cable")
+  i += 1
--- main.py
+++
@@ -1,8 +0,0 @@
-print("What did I hear?")
-s = input()
-print("What did I see?")
-im = input()
-if ((s == "grrr") and (im == "two red eyes")):
-  print("There is a scary creature. I should get out of here!")
-else:
-  print("I am a little scared but I will continue.")
